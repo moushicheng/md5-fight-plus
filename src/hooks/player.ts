5ea70527215b65d8f2244555297ba0a87cd3c063
moushicheng <moushicheng@outlook.com>
2023-05-17 18:16:18 +0800

    feat: remove player hook

--- src/hooks/player.ts
+++
@@ -1,9 +0,0 @@
-import { PlayerInstanceProperty } from "src/types/player"
-import { SyncBailHook } from "./SyncBailHook"
-
-export const createPlayerHook = () => {
-    const hooks = {
-        initProperty: new SyncBailHook<PlayerInstanceProperty>()
-    }
-    return hooks
-}
