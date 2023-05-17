41db7afc635f6e9cc954fc7499c8dca7a8da9400
moushicheng <moushicheng@outlook.com>
2023-05-17 18:18:00 +0800

    feat: 注册一些tap

--- src/battle-field/registerHooks.ts
+++ src/battle-field/registerHooks.ts
@@ -1,5 +1,11 @@
 import { BattleFieldInstance } from "@/types/battleField";
+import { getPlayers } from "@/utils";
 
 export function registerHooks(battleField: BattleFieldInstance) {
+    battleField.hooks.fightStart.tap('calculate runtimeProperty', (battleField) => {
+        const { player1, player2 } = getPlayers(battleField)
+    })
+    battleField.hooks.fightStart.tap('choose skills', (battleField) => {
 
+    })
 }
--- src/battle-field/registerRoundHooks.ts
+++ src/battle-field/registerRoundHooks.ts
@@ -1,15 +1,12 @@
-import { BailEvent } from "@/hooks/SyncBailHook";
+import { checkPlayerDeath } from "@/hooks/intercepts/checkPlayer";
 import { BattleFieldInstance } from "@/types/battleField";
 
 export function registerRoundHooks(battleField: BattleFieldInstance) {
-    battleField.roundHooks.roundReady.registerIntercept((params) => {
-        //假设有玩家死亡
-        const hp = 0
-
-
+    battleField.roundHooks.roundReady.registerIntercept((battleField) => {
+        checkPlayerDeath(battleField)
     })
     battleField.roundHooks.roundReady.tap('Order', (battleField) => {
-        return new BailEvent('1')
+        return battleField
     })
 
 }
