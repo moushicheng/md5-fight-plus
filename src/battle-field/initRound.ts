cc402075fdba073703f79d5acd4037b51053ac98
moushicheng <moushicheng@outlook.com>
2023-05-19 13:25:03 +0800

    feat: 添加核心流程

--- src/battle-field/initRound.ts
+++ src/battle-field/initRound.ts
@@ -1,3 +1,4 @@
+import { PlayerDeathEvent } from "@/events/playerDeath";
 import { SyncBailHook } from "@/hooks/SyncBailHook";
 import { BattleFieldInstance } from "@/types/battleField";
 
@@ -8,9 +9,23 @@ export function initRound(battleField: BattleFieldInstance) {
             battleField.roundHooks.roundStart.call(battleField)
             const attacker = battleField.players.left;
             const defender = battleField.players.right;
-            //attacker准备阶段
+            //准备阶段
             attacker.hooks.prepare.call(battleField)
+            //攻击前
+            attacker.hooks.beforeAttack.call(battleField)
+            defender.hooks.beforeUnderAttacked.call(battleField)
+            //攻击时
+            const damage = attacker.hooks.onAttack.call(battleField)
+            defender.hooks.onUnderAttack.call({ battleField, damage });
+            //攻击后
+            attacker.hooks.afterAttack.call(battleField);
+            defender.hooks.afterUnderAttack.call(battleField)
+            //回合结束阶段
+            battleField.roundHooks.roundEnd.call(battleField)
         } catch (err) {
+            if (err instanceof PlayerDeathEvent) {
+                console.log('游戏结束，玩家x死亡');
+            }
         }
     }
     battleField.roundHooks = {
