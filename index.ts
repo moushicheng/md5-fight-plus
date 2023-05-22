fb0a9c1686a58cf17a8ed4d2f4ba68bd5843e697
moushicheng <moushicheng@outlook.com>
2023-05-22 10:29:38 +0800

    feat: 再次修改回合模型

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 import { createBattleField } from "@/battle-field";
 import { createPlayer } from "./src/player";
 import { Level } from "@/logs";
+import { hooksRecord } from "@/hooks/SyncBailHook";
 
 const p1 = createPlayer('p1')
 const p2 = createPlayer('p2')
@@ -9,3 +10,5 @@ const battleField = createBattleField(p1, p2)
 battleField.fight()
 
 console.log(battleField.logger.getByLevel(Level.info).map(item => item.message).join('\n'));
+console.log(hooksRecord.slice(0, 20).join('\n'));
+battleField.logger.addDebug(hooksRecord.join('\n'))
