c6392ce9fba587aa5b30b8353e0830820638090b
moushicheng <moushicheng@outlook.com>
2023-05-22 17:35:42 +0800

    feat: 拦截器改名

--- src/battle-field/registerIntercept.ts
+++ src/battle-field/registerIntercept.ts
@@ -3,7 +3,7 @@ import { BattleFieldInstance } from "@/types/battleField";
 import { PlayerInstanceProperty } from "@/types/player";
 import { getPlayers } from "@/utils";
 
-export function registerIntercept(battleField: BattleFieldInstance) {
+export function registerAfterActionHook(battleField: BattleFieldInstance) {
     const { player1, player2 } = getPlayers(battleField);
     register(player1, battleField)
     register(player2, battleField)
@@ -12,7 +12,7 @@ function register(player: PlayerInstanceProperty, battleField: BattleFieldInstan
     const hooks = player.hooks
     for (const key in hooks) {
         const hook = hooks[key];
-        hook.registerIntercept('check Death', (props) => {
+        hook.registerAfterActionHook('check Death', (props) => {
             const deathEvent = checkPlayerDeath(battleField)
             if (deathEvent) throw deathEvent.message
             return props
