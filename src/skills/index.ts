386e21850508e45287a62c5bebf3bcafd52b87a4
moushicheng <moushicheng@outlook.com>
2023-05-25 22:50:41 +0800

    feat: 蓝耗

--- src/skills/index.ts
+++ src/skills/index.ts
@@ -1,8 +1,16 @@
 
+import { preprocessSkill } from '@/utils'
 import { snowball } from './frostbite'
 import { normalAttack } from './normal-attack'
 
-export default {
+const raw_skills = {
     normalAttack: normalAttack,
     snowball: snowball
 }
+Object.keys(raw_skills).forEach(key => {
+    const skill = raw_skills[key]
+
+    skill.run = preprocessSkill(skill)
+})
+
+export default raw_skills
