e7cfeb6ecebd45a8410103e5c6a0faefcc459a5c
moushicheng <moushicheng@outlook.com>
2023-05-28 10:18:47 +0800

    feat: 添加技能绝对零度

--- index.ts
+++ index.ts
@@ -5,10 +5,7 @@ import { Level } from "@/logs";
 
 const p1 = createPlayer('p3')
 const p2 = createPlayer('p2')
-p2.skills[0] = 'snowball'
-p2.skills[1] = 'snowball'
-p2.skills[2] = 'snowball'
-p2.skills[3] = 'blizzard'
+p2.skills[0] = 'absoluteZero'
 console.log('@创建角色成功', p1.name, p1.baseProperty);
 console.log('@创建角色成功', p2.name, p2.baseProperty);
 const battleField = createBattleField(p1, p2)
--- src/skills/index.ts
+++ src/skills/index.ts
@@ -3,15 +3,16 @@ import { preprocessSkill } from '@/utils'
 import { snowball } from './frostbite'
 import { normalAttack } from './normal-attack'
 import { blizzard } from './blizzard'
+import { absoluteZero } from './absoluteZero'
 
 const raw_skills = {
-    normalAttack: normalAttack,
-    snowball: snowball,
-    blizzard: blizzard
+    normalAttack,
+    snowball,
+    blizzard,
+    absoluteZero,
 }
 Object.keys(raw_skills).forEach(key => {
     const skill = raw_skills[key]
-
     skill.run = preprocessSkill(skill)
 })
 
