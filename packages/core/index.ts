4dc2022ac9c391bba3901ee0ace5251932456c66
moushicheng <moushicheng@outlook.com>
2023-07-07 19:27:32 +0800

    test

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -5,7 +5,7 @@ import { Level } from "@/logs";
 
 const p1 = createPlayer('p3')
 const p2 = createPlayer('p2')
-p1.skills = ['fireBlast', 'fireElf', 'a', 'a', 'a']
+p1.skills = ['fireBlast', 'fireElf', 'a', 'a', 'ab']
 console.log('@创建角色成功', p1.name, p1.baseProperty);
 console.log('@创建角色成功', p2.name, p2.baseProperty);
 const battleField = createBattleField(p1, p2)
