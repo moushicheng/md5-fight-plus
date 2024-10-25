d7c2f5239a802cc9dbb34fb4dca89345839defef
moushicheng <moushicheng@outlook.com>
2024-10-25 12:04:45 +0800

    chore: remove test

--- packages/core/test.ts
+++ packages/core/test.ts
@@ -1,9 +1,12 @@
-// import { createBattleField, createPlayer } from "./index";
-// import { LogLevel } from "./index";
 // const p1 = createPlayer("p3");
 // const p2 = createPlayer("p2");
 // p1.skills = ["fireBlast", "fireElf", "a", "a", "ab"];
-// console.log("@创建角色成功", p1.name, p1.baseProperty);
+// console.log(
+//   "@创建角色成功",
+//   p1.name,
+//   p1.baseProperty,
+//   p1.runtimeProperty.firing
+// );
 // console.log("@创建角色成功", p2.name, p2.baseProperty);
 // const battleField = createBattleField(p1, p2);
 // battleField.fight();
@@ -11,6 +14,8 @@
 // console.log(
 //   battleField.logger
 //     .getByLevel(LogLevel.info)
-//     .map((item) => item.message)
+//     .map((item) => {
+//       return item.message + "p3:" + item.players.p3?.runtimeProperty?.firing;
+//     })
 //     .join("\n")
 // );
